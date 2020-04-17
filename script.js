0a29a7a43dee64cc5194a6976cee6c135023e85b
Harrisiqbal5 <48575731+Harrisiqbal5@users.noreply.github.com>
2020-04-17 00:57:56 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+
+
+
+const puppeteer = require('puppeteer');
+
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = process.env.PORT || 3002
+
+app.use(express.static(path.join(__dirname,'my-app/build')))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+async function asyncCall()  {
+        // const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            args: ['--user-agent=<user_agent_string>'],
+            });
+
+
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1920, height: 926 });
+        await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36')
+        await page.goto('https://www.costco.com/CatalogSearch?dept=All&keyword=toilet+paper');
+
+    let hotelData = await page.evaluate(() => {
+      let hotels = [];
+      // get the hotel elements
+      let hotelsElms = document.querySelectorAll('div.product div.product-tile-set');
+      // get the hotel data
+      let count = 0
+      hotelsElms.forEach((hotelelement) => {
+          let hotelJson = {};
+          try {
+            
+            hotelJson.name = hotelelement.querySelector("img.img-responsive").getAttribute(['alt']);
+            hotelJson.quantity = hotelelement.querySelector("input[name='in_Stock']").getAttribute(['value']);
+
+          // hotelJson.status = hotelelement.querySelector("img.product-out-stock-overlay").getAttribute(['title']);
+
+          try {
+            hotelJson.status = hotelelement.querySelector("img.product-out-stock-overlay").getAttribute(['title'])
+
+          }catch (exception){
+            hotelJson.status = 'Unknown'
+
+          }
+            hotelJson.Method = "Regular"
+            hotelJson.Buy = "No"  
+            hotelJson.Method = hotelelement.querySelector("img.hom-bad2").getAttribute(['alt'])
+
+            if (hotelelement.querySelector("input[name='in_Stock']").getAttribute(['value']) > 0 && hotelJson.Method == "Regular"){
+              hotelJson.Buy = "Yes" 
+              count = count + 1
+
+            } 
+
+            
+          }
+          catch (exception){
+
+          }
+          hotels.push(hotelJson);
+      });
+      // return hotels;
+      return [hotels,count];
+  });
+
+  current_time = (Date()).toString();
+  console.log(current_time)
+  console.dir(JSON.stringify(hotelData));
+  console.dir(hotelData.length)
+
+  console.dir(JSON.stringify(hotelData[0]));
+  console.dir(hotelData[0].length)
+  console.log(hotelData[1])
+  if (hotelData[1] > 0){
+    send_mail()
+  }
+
+  app.post('/getdata', function (req, res){
+    res.send(JSON.stringify(hotelData[0]))
+  })
+  app.post('/gettime', function (req, res){
+    res.send(current_time)
+  })
+  
+
+
+//// working part end
+        
+        // const myLocalValue = '12345';
+        // // await page.$eval('input[name=CN1]', (el, value) => el.value = value, myLocalValue);
+        // await page.type('input[name=CN1]', 'ssss', {delay: 20})
+        await page.screenshot({path: 'sccse.png'});
+        await browser.close();
+      
+      };
+
+      asyncCall()
+      var minutes = 5, the_interval = minutes * 60 * 1000;
+      setInterval(function() {
+        asyncCall()
+      }, the_interval);
+
+      
